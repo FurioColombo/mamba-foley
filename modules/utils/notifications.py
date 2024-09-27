6cc76a2f11da5cf8d3afae07786ee0cf29e8b05c
Marco Furio Colombo <48253359+FurioColombo@users.noreply.github.com>
2024-09-27 11:38:33 +0200

    Update notifications.py
    remove secret

--- modules/utils/notifications.py
+++ modules/utils/notifications.py
@@ -1,15 +1,18 @@
 import requests
+import 
 
-def notify_telegram(message, verbose=False):
-    apiToken = '7133523436:AAHSM_44T1MFKLhkHxm5myZ7u0o1IE0Xc6Q'
-    chatID = '275894223'
+def notify_telegram(message, config, verbose=False):
+    tg_config = config.telegram if hasattr(config, 'telegram') else config
+    apiToken = tg_config.apiToken
+    chatID = tg_config.chatID
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
-
-    try:
-        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
-        if verbose:
-            print(f'Telegram message response: {response}.')
-        if response.ok is not True:
-            print(f'TELEGRAM NOTIFICATION ERROR: response is not OK: {str(response)}.')
-    except Exception as e:
-        print('Telegram notification Exception:', e)
+    
+    if apiToken != '' and chatID != ''
+        try:
+            response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+            if verbose:
+                print(f'Telegram message response: {response}.')
+            if response.ok is not True:
+                print(f'TELEGRAM NOTIFICATION ERROR: response is not OK: {str(response)}.')
+        except Exception as e:
+            print('Telegram notification Exception:', e)
